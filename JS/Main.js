8cc1c189ac58785bf0f494fdeec41095c970aeb3
Tusar kumar Parida <143640625+tusarxkumar@users.noreply.github.com>
2024-06-28 21:25:14 +0530

    initial commit

---
+++ JS/Main.js
@@ -0,0 +1,70 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+    });
+    ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+    ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form' , { origin: 'buttom' });
+    ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+    ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+
+    // typed js 
+    const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', '4th Year Student'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+    });
+
+    
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxZjSaGU97TV6LeDrHVA2Hjqwx6i2GI1bHNJPH3VQySw7WfFih4LCijdktJLKUg-5eJNg/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById("msg")
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, {method: 'POST', body: new FormData(form)})
+            .then(response => {
+                msg.innerHTML = "Message sent successfully"
+                setTimeout(function(){
+                    msg.innerHTML = ""
+                },5000)
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error.message))
+    })
