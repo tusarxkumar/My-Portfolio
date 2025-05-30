4264e53d14b865a7d0b7dbc32f692a9d2baac681
Tusar kumar Parida <tusarkumarparida9@gmail.com>
2025-05-30 12:52:50 +0530

    fixed contact me

--- JS/Main.js
+++ JS/Main.js
@@ -52,22 +52,7 @@ const typed = new Typed('.multiple-text', {
 });
 
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxZjSaGU97TV6LeDrHVA2Hjqwx6i2GI1bHNJPH3VQySw7WfFih4LCijdktJLKUg-5eJNg/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => {
-            msg.innerHTML = "Message sent successfully"
-            setTimeout(function () {
-                msg.innerHTML = ""
-            }, 5000)
-            form.reset()
-        })
-        .catch(error => console.error('Error!', error.message))
-})
 
     var loader = document.getElementById("preloader");
     window.addEventListener("load", function () {
